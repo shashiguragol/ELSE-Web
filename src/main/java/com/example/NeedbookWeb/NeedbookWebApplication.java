2f692e795a81b9b86b351e313f5f7066d0db552d
shashiguragol <shashi.guragol@gmail.com>
2019-11-06 23:32:24 +0530

    first commit

---
+++ src/main/java/com/example/NeedbookWeb/NeedbookWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.NeedbookWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedbookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedbookWebApplication.class, args);
+	}
+
+}
